aa6487871eefc103d34c57c46c44d23a48e3d48e
reddog34 <74236811+reddog34@users.noreply.github.com>
2020-12-09 15:53:16 +0100

    shortened some code

--- Eigen Game Proftaak/Assets/Scripts/PlayerDeath.cs
+++ Eigen Game Proftaak/Assets/Scripts/PlayerDeath.cs
@@ -9,6 +9,7 @@ public class PlayerDeath : MonoBehaviour
     public Tilemap DeathTiles;
     public ContactPoint2D[] contacts = new ContactPoint2D[1];
     public Vector3 respawnPoint;
+    public HealthManger playerHealth;
 
 
     private void Start()
@@ -34,15 +35,16 @@ public class PlayerDeath : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy")|| collision.gameObject.tag == "Checkpoint")
         {
             // Destroy(gameObject);
             transform.position = respawnPoint;
+            //playerHealth.damage(1);
         }
-        if (collision.gameObject.tag == "Checkpoint")
-        {
-            // LevelManager.instance.Respawn(); 
-            respawnPoint = collision.transform.position;
-        }
+        //if (collision.gameObject.tag == "Checkpoint")
+        //{
+        //    // LevelManager.instance.Respawn(); 
+        //    respawnPoint = collision.transform.position;
+        //}
     }
 }
