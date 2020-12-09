573c76a287a9a5e4d9584c69785f4d2d89e2bee6
reddog34 <74236811+reddog34@users.noreply.github.com>
2020-12-09 15:50:44 +0100

    added arduino handeling

---
+++ Eigen Game Proftaak/Assets/Scripts/ArduinoMessageHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System;
+using UnityEngine;
+
+public class ArduinoMessageHandler : MonoBehaviour
+{
+    //port fields
+    private SerialPort myPort;
+    private string portName = "COM4";
+    private char startChar = '#';
+    private char endChar = '%';
+
+
+    //Constructor
+    private ArduinoMessageHandler()
+    {
+        startPortCommunication();
+    }
+
+    //sends message to serial output
+    public void sendMessage(string message)
+    {
+        if (myPort.IsOpen)
+        {
+            myPort.Write(startChar + message + endChar); //"#AN_EXAMPLE_MESSAGE%"
+        }
+    }
+
+    private void startPortCommunication()
+    {
+        try
+        {
+            myPort = new SerialPort();
+            myPort.BaudRate = 9600;
+            myPort.PortName = this.portName;
+            myPort.Open();
+        }
+        catch (Exception)
+        {
+            UnityEngine.Debug.Log("could not connect to arduino port");
+        }
+    }
+}
